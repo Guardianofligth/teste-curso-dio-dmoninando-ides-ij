301f7166e09b13bf6ad4d638828ceb969bfeface
Guardianofligth <guardianofligth@outlook.com>
2022-01-19 02:01:52 -0300

    first commit

---
+++ src/br/vom/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.vom.dio;
+
+import br.vom.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+
+        /* int a = 5;
+         int b = 2;
+
+
+        System.out.println("hello wordl!!" + (a+b));*/
+
+
+    }
+}
